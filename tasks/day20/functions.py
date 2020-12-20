cfd30c91a0f9f573dbe710965ceaae2fa44b0c99
Ales <oxudes@gmail.com>
2020-12-20 18:38:08 +0100

    day20 task 1 - just took me around 8h, i suck at this

--- tasks/day20/functions.py
+++ tasks/day20/functions.py
@@ -3,11 +3,24 @@ from math import sqrt
 from itertools import combinations, product
 
 Coords = tuple[int, int]
-Edge = list[int]
 ProcessOrder = list[list[Coords]]
 
+mp = {
+  (0,0): '1951',
+  (1,0): '2311',
+  (2,0): '3079',
+
+  (0,1): '2729',
+  (1,1): '1427',
+  (2,1): '2473',
+
+  (0,2): '2971',
+  (1,2): '1489',
+  (2,2): '1171',
+}
+
 TT = 0 # top edge
-RR = 1 # right edge
+RR = 1 # right edge4
 BB = 2 # bottom edge
 LL = 3 # left edge
 edge_map = {
@@ -22,47 +35,58 @@ class Image:
     self.id = img_id
     self.content = content
     l_x, l_y = len(content[0]), len(content)
-    edges = [
-      [xx for xx, val in enumerate(content[0]) if val == "#"], # top
-      [yy for yy, row in enumerate(content) if row[-1] == "#"], # right
-      [xx for xx, val in enumerate(content[-1]) if val == "#"], # bottom
-      [yy for yy, row in enumerate(content) if row[0] == "#"], # left
+    if l_x != l_y:
+      raise Exception("Mismatches image dimensions")
+
+    edges = {
+      TT: content[0], # top
+      RR: "".join([row[-1] for yy, row in enumerate(content)]), # right
+      BB: content[-1], # bottom
+      LL: "".join([row[0] for yy, row in enumerate(content)]), # left
+    }
+
+    self.combos = [
+      { TT: edges[TT][:], RR: edges[RR][:], BB: edges[BB][:], LL: edges[LL][:] },
+      { TT: edges[BB][:], RR: edges[RR][::-1], BB: edges[TT][:], LL: edges[LL][::-1] }, # X-axis flip
+      { TT: edges[TT][::-1], RR: edges[LL][:], BB: edges[BB][::-1], LL: edges[RR][:] }, # Y-axis flip
+      { TT: edges[BB][::-1], RR: edges[LL][::-1], BB: edges[TT][::-1], LL: edges[RR][::-1] }, # double axis flip
     ]
 
-    self.combos: list[Edge] = [
-      deepcopy(edges),
-      [deepcopy(edges[BB]), deepcopy(edges[RR]), deepcopy(edges[TT]), deepcopy(edges[LL])], # X-axis flip
-      [deepcopy(edges[TT]), deepcopy(edges[LL]), deepcopy(edges[BB]), deepcopy(edges[RR])], # Y-axis flip
-      [deepcopy(edges[BB]), deepcopy(edges[LL]), deepcopy(edges[TT]), deepcopy(edges[RR])], # double axis flip
-    ]
+    expected = {
+      TT: "..#.###...",
+      BB: ".##...####",
+      RR: ".####....#",
+      LL: ".#.#.###.."
+    }
 
-    # manually reverse the relevant edges
-    self.combos[1][LL].reverse()
-    self.combos[1][RR].reverse()
+    for jj in range(4):
+      tmp = deepcopy(self.combos[jj])
+      for ii in range(1,4):
+        tmp = { (xx + 1) % 4: tmp[xx] for xx in tmp }
+        tmp[TT] = tmp[TT][::-1]
+        tmp[BB] = tmp[BB][::-1]
+        if tmp not in self.combos:
+          self.combos.append(tmp)
 
-    self.combos[2][TT].reverse()
-    self.combos[2][BB].reverse()
 
-    self.combos[3][TT].reverse()
-    self.combos[3][BB].reverse()
-    self.combos[3][LL].reverse()
-    self.combos[3][RR].reverse()
+    self.active = self.combos[0]
 
-    self.rotation = 0
-    self.cfg = 0
-
-  def get_edge(self, direction: int):
-    return self.combos[self.cfg][(direction + self.rotation) % 4]
+  def get_edge(self, direction):
+    return self.active[direction]
 
   def fits(self, reqs: dict[int, list[int]]):
     matches = []
-    for rotation in range(4):
-      for cfg, edges in enumerate(self.combos):
-        layout = { req_dir: edges[(req_dir + rotation) % 4] for req_dir in reqs }
-        if layout == reqs:
-          matches.append((rotation, cfg))
+    for layout in self.combos:
+      if reqs == { req_dir: layout[req_dir] for req_dir in reqs }:
+        matches.append(layout)
 
     return matches
+
+  def lock(self, orientation):
+    if orientation not in self.combos:
+      raise Exception("invalid orientation used")
+    self.active = orientation
+    return self
 
 
 class Collection:
@@ -80,56 +104,71 @@ class Collection:
       raise Exception("Invalid input")
 
     self.coords: list[Coords] = list(product(range(0, dimension), range(0, dimension)))
-    self.neighbours: dict[Coords, list[Coords]] = {}
+    self.neighbours: dict[Coords, dict[int, Coords]] = {}
+    self.max = dimension - 1
+
     for node in self.coords:
       xx, yy = node
       self.neighbours[node] = {pos: node for pos, node in {LL: (xx-1, yy), RR: (xx+1, yy), TT: (xx, yy-1), BB: (xx, yy+1)}.items() if node in self.coords}
-
     center = (int(dimension/2), int(dimension/2))
     # We can always rotate this one with the entire composition if it matches, so its orientation does not matter
     self.coords.remove(center)
 
     order = self.get_order(center)
-    self.full_check(center, order)
+    return self.full_check(center, order)
 
   def get_order(self, start: Coords) -> ProcessOrder:
     order = []
-    used = [start]
     limit = len(self.coords) + 1
 
     todo = [start]
     while len(todo) > 0:
       node = todo.pop(0)
-      new_list = [xx for xx in self.neighbours[node].values() if xx not in used]
-      if len(new_list) < 1:
-        continue
-      order.append(new_list)
-      todo += order[-1]
-      used += order[-1]
+      for ngb in self.neighbours[node].values():
+        if ngb not in order:
+          order.append(ngb)
+          todo.append(ngb)
+
+    order.remove(start)
 
     return order
 
   def full_check(self, center: Coords, order: ProcessOrder):
     for parent in self.all:
-      parent.rotation = 0
-      layout = {
-        center: parent
-      }
-      self.rec_check(layout, order)
-      exit()
+      for src in parent.combos:
+        layout = {
+          center: parent.lock(src),
+        }
+        correct = self.rec_check(layout, order)
+        if correct:
+          xx = self.max
+          aa = int(correct[(0, 0)].id)
+          bb = int(correct[(xx, 0)].id)
+          cc = int(correct[(0, xx)].id)
+          dd = int(correct[(xx, xx)].id)
+          return aa * bb * cc * dd
 
-  def rec_check(self, layout: dict[Coords, Image], order: ProcessOrder, ord_step = 0, ord_pos = 0):
-    remaining: list[Image] = [node for node in self.all if node not in layout.keys()]
+  def rec_check(self, layout: dict[Coords, Image], order: ProcessOrder, step = 0):
+    remaining = [node for node in self.all if node not in layout.values()]
+    if step >= len(order):
+      return layout
+    pos = order[step]
 
-    pos = order[ord_step][ord_pos]
     for img in remaining:
       fits = self.possible_fits(layout, pos, img)
-      print(fits)
-      exit()
+      if len(fits) < 1:
+        # img cannot fit into this position
+        continue
+
+      for fit in fits:
+        l2 = layout.copy()
+        l2[pos] = img.lock(fit)
+        res = self.rec_check(l2, order, step + 1)
+        if res:
+          return res
 
   def possible_fits(self, layout: dict[Coords, Image], node: Coords, img: Image):
     reqs = {}
-
     for edge_id, nei in self.neighbours[node].items():
       if nei not in layout:
         # Nothing there yet, automatic pass
